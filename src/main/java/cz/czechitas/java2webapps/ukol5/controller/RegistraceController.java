8dc23c451b3e63f4951f80d3cb5d41b5b54e8663
Nix766 <sarkadlouha89@gmail.com>
2021-05-09 17:16:41 +0200

    registrace na tabor

--- src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
@@ -1,7 +1,17 @@
 package cz.czechitas.java2webapps.ukol5.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.EnumSet;
 
 /**
  * Kontroler obsluhující registraci účastníků dětského tábora.
@@ -9,9 +19,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RegistraceController {
 
-  @GetMapping("/")
-  public String formular() {
-    return "formular";
+  @GetMapping("")
+  public ModelAndView index() {
+    ModelAndView modelAndView = new ModelAndView("/formular");
+    modelAndView.addObject("form", new RegistraceForm());
+    return modelAndView;
   }
+
+  @PostMapping("")
+  public Object form(@ModelAttribute("form") @Valid RegistraceForm form, BindingResult bindingResult) {
+
+    if (bindingResult.hasErrors()) {
+      return "/formular";
+    }
+
+    if (form.getSport().size() < 2) {
+      System.out.println(form.getSport().size());
+      bindingResult.rejectValue("sport", "", "Prosím vyberte alespoň 2 sporty.");
+      return "/formular";
+
+    }
+
+    int vek = form.getVek();
+    if (vek < 9 || vek > 15) {
+
+      bindingResult.rejectValue("datumNarozeni", "", "Tábory pořádáme jen pro děti mezi 9 - 15 lety.");
+      return "/formular";
+
+    }
+
+    return new ModelAndView("/registrovano")
+            .addObject("jmeno", form.getJmeno())
+            .addObject("prijmeni", form.getPrijmeni())
+            .addObject("datumNarozeni", form.getDatumNarozeni())
+            .addObject("turnus", form.getTurnusObdobi())
+            .addObject("pohlavi", form.getPohlavi())
+            .addObject("sport", form.getSport())
+            .addObject("email", form.getEmail())
+            .addObject("telefon", form.getTelefon());
+
+
+  }
+
 
 }
---
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceForm.java
@@ -0,0 +1,108 @@
+package cz.czechitas.java2webapps.ukol5.controller;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.EnumSet;
+
+public class RegistraceForm {
+//    @NotBlank
+    private String jmeno;
+//    @NotBlank
+    private String prijmeni;
+//    @NotNull
+    private LocalDate datumNarozeni;
+//    @NotBlank
+    private String turnus;
+//    @NotNull
+    private Pohlavi pohlavi;
+//    @NotNull
+    private EnumSet<Sport> sport;
+    private String email;
+    private String telefon;
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public String getPrijmeni() {
+        return prijmeni;
+    }
+
+    public void setPrijmeni(String prijmeni) {
+        this.prijmeni = prijmeni;
+    }
+
+    public LocalDate getDatumNarozeni() {
+        return datumNarozeni;
+    }
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    public void setDatumNarozeni(LocalDate datumNarozeni) {
+        this.datumNarozeni = datumNarozeni;
+    }
+
+    public String getTurnus() {
+        return this.turnus;
+    }
+
+    public void setTurnus(String turnus) {
+        this.turnus = turnus;
+    }
+
+    public Pohlavi getPohlavi() {
+        return pohlavi;
+    }
+
+    public void setPohlavi(Pohlavi pohlavi) {
+        this.pohlavi = pohlavi;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public int getVek() {
+        Period period = datumNarozeni.until(LocalDate.now());
+        return period.getYears();
+    }
+
+    public void setSport(EnumSet<Sport> sport) {
+        this.sport = sport;
+    }
+
+    public EnumSet<Sport> getSport() {
+        return this.sport;
+    }
+
+    public String getTurnusObdobi() {
+        switch(this.turnus) {
+            case "1":
+                return "5. - 11. července";
+            case "2":
+                return "12. - 18. července";
+            default:
+                return "19. - 25. července";
+        }
+    }
+
+}
